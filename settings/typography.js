1307ac347c98507483922967704aea7bf009898d
AMangano <andreaman87@gmail.com>
2017-02-08 21:15:20 +0100

    Adds external settings file for typography

---
+++ settings/typography.js
@@ -0,0 +1,66 @@
+module.exports = {
+  h1: {
+    sizes: {
+      medium: 43,
+      large: 48
+    },
+    'font-size': 34,
+    'font-weight': 'normal',
+    'line-height': 1.2
+  },
+  h2: {
+    sizes: {
+      medium: 34,
+      large: 36
+    },
+    'font-size': 30,
+    'font-weight': 'normal',
+    'line-height': 1.2
+  },
+  h3: {
+    sizes: {
+      medium: 24
+    },
+    'font-size': 22,
+    'font-weight': 'normal',
+    'line-height': 1.3
+  },
+  h4: {
+    sizes: {
+      medium: 20,
+      large: 21
+    },
+    'font-size': 19,
+    'font-weight': 'bold',
+    'line-height': 1.2
+  },
+  h5: {
+    sizes: {
+      medium: 18,
+      large: 16
+    },
+    'font-size': 17,
+    'font-weight': 'normal',
+    'line-height': 1.2
+  },
+  h6: {
+    sizes: {
+      large: 14
+    },
+    'font-size': 16,
+    'font-weight': 'normal',
+    'line-height': 1.3
+  },
+  p: {
+    'font-size': 16,
+    'font-weight': 'normal',
+    'line-height': 1.4
+  },
+  small: {
+    sizes: {
+      medium: 14
+    },
+    'font-size': 12,
+    'font-weight': 'normal'
+  },
+};
