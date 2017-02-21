021f6fcff0fc8b7e4349f9df62aab695841a0c96
AMangano <andreaman87@gmail.com>
2017-02-21 23:28:57 +0100

    Adds docs navigation list

--- settings/pug.js
+++ settings/pug.js
@@ -1,4 +1,9 @@
 var data = require('./../dist/data.json');
+data.navigation = [
+  {label: 'Logo', url: '#logo'},
+  {label: 'Palettes', url: '#palettes'},
+  {label: 'Typography', url: '#typography'}
+];
 data.palettes = require('./palette');
 data.typography = require('./typography');
 module.exports = data;
