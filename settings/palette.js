e88609f9639275a0111e797dfe525826fe8ae837
Andrea Mangano <andreaman87@gmail.com>
2017-02-22 22:16:50 +0100

    Improves the UI atomization and the relative documentation (#13)
    * ITCSS structure refactoring
    
    * Improves logo versions and logotype
    
    * Removes unused style
    
    * Fix lint
    
    * Moves footer and header in "layout" level
    
    * Makes the text container larger
    
    * Fix lint
    
    * Improves docs style
    
    * Adds colors to brand palette
    
    * Adds docs navigation list
    
    * Improves footer
    
    * Adds doc section for logos
    
    * Adds vertical logo version
    
    * Fix layout docs
    
    * Improves docs mixins
    
    * Creates placeholder for buttons
    
    * Creates separated components "faq"
    
    * Renames the faq list
    
    * Improves style colour palettes
    
    * Adds faq list component dependency
    
    * Rearranges brand colour palette
    
    * Adds new docs sections to the navigation
    
    * Organises layout components into mixins and separates templates
    
    * Adds new docs sections and improves the older ones
    
    * Reformats code and adds website mixins
    
    * Changes structure of color-box block
    
    * Uses templates and mixins to make the view modularized
    
    * Improves style for footer and faqs

--- settings/palette.js
+++ settings/palette.js
@@ -1,16 +1,16 @@
 module.exports = {
   brand: {
-    primary: '#ffd300'
+    light: '#ffe66d',
+    primary: '#ffd300',
+    dark: '#927900'
   },
   neutral: {
-    'lowest': '#fff',
-    'lower': '#d1d7db',
-    'low': '#a3b0b8',
-    'medium': '#758894',
-    'high': '#5b6a74',
-    'higher': '#262e34',
-    'highest': '#0c0c0c'
+    lowest: '#fff',
+    lower: '#d1d7db',
+    low: '#a3b0b8',
+    medium: '#758894',
+    high: '#5b6a74',
+    higher: '#262e34',
+    highest: '#0c0c0c'
   }
 };
-
-
--- settings/pug.js
+++ settings/pug.js
@@ -1,4 +1,12 @@
 var data = require('./../dist/data.json');
+data.navigation = [
+  {label: 'Logo', url: '#logo'},
+  {label: 'Palettes', url: '#palettes'},
+  {label: 'Typography', url: '#typography'},
+  {label: 'Icons', url: '#icons'},
+  {label: 'Faq', url: '#faq'},
+  {label: 'Footer', url: '#footer'}
+];
 data.palettes = require('./palette');
 data.typography = require('./typography');
 module.exports = data;
