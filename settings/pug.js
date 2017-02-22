7ce2424c29ab59ded6eb1366be5743d6117f775e
AMangano <andreaman87@gmail.com>
2017-02-22 20:49:33 +0100

    Adds new docs sections to the navigation

--- settings/pug.js
+++ settings/pug.js
@@ -2,7 +2,10 @@ var data = require('./../dist/data.json');
 data.navigation = [
   {label: 'Logo', url: '#logo'},
   {label: 'Palettes', url: '#palettes'},
-  {label: 'Typography', url: '#typography'}
+  {label: 'Typography', url: '#typography'},
+  {label: 'Icons', url: '#icons'},
+  {label: 'Faq', url: '#faq'},
+  {label: 'Footer', url: '#footer'}
 ];
 data.palettes = require('./palette');
 data.typography = require('./typography');
