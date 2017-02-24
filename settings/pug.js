54e4472167513b559428993902d6315abc474e54
AMangano <andreaman87@gmail.com>
2017-02-24 21:09:49 +0100

    Adds two levels navigation (doc)

--- settings/pug.js
+++ settings/pug.js
@@ -1,11 +1,27 @@
 var data = require('./../dist/data.json');
 data.navigation = [
-  {label: 'Logo', url: '#logo'},
-  {label: 'Palettes', url: '#palettes'},
-  {label: 'Typography', url: '#typography'},
-  {label: 'Icons', url: '#icons'},
-  {label: 'Faq', url: '#faq'},
-  {label: 'Footer', url: '#footer'}
+  {
+    label: 'Basic',
+    items: [
+      {label: 'Logo', url: '#logo'},
+      {label: 'Palettes', url: '#palettes'},
+      {label: 'Typography', url: '#typography'},
+      {label: 'Icons', url: '#icons'}
+    ]
+  },
+  {
+    label: 'Components',
+    items: [
+      {label: 'Faq', url: '#faq'}
+    ]
+  },
+  {
+    label: 'Layouts',
+    items: [
+      {label: 'Header', url: '#header'},
+      {label: 'Footer', url: '#footer'}
+    ]
+  }
 ];
 data.palettes = require('./palettes');
 data.typography = require('./typography');
