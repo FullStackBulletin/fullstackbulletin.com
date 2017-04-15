62e5a13cbdd2d2352237d4c77664c19c419ae39c
AMangano <andreaman87@gmail.com>
2017-04-15 21:42:33 +0200

    Updates to webpack 2

--- webpack.development.js
+++ webpack.development.js
@@ -1,26 +1,23 @@
-// const webpack = require('webpack');
 const path = require('path');
-const BUILDDIR = path.resolve(__dirname, 'dist');
+const BUILDDIR = path.resolve(__dirname, process.env['npm_package_per_env_development_output_folder']);
 
 module.exports = [{
   name: 'client',
   entry: {
     main: path.resolve(__dirname, 'assets/scripts/main.js')
   },
-  target: 'node-webkit',
   output: {
     path: path.resolve(BUILDDIR, 'js'),
     filename: '[name].bundle.js'
   },
-  externals: {
-    jquery: '$'
-  },
+  context: __dirname,
+  target: 'web',
   module: {
     loaders: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel',
+        loader: 'babel-loader',
         query: {
           presets: ['es2015']
         }
