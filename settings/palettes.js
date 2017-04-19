ffaebcff6154b9926d2ce87a8b4d378f797ea843
AMangano <andreaman87@gmail.com>
2017-04-20 00:54:39 +0200

    Updates colours palette

--- settings/palettes.js
+++ settings/palettes.js
@@ -1,16 +1,17 @@
 module.exports = {
   brand: {
     light: '#ffe66d',
-    primary: '#ffd300',
+    primary: '#f8e81c',
+    // primary: '#ffd300',
     dark: '#927900'
   },
   neutral: {
     lowest: '#fff',
-    lower: '#d1d7db',
-    low: '#a3b0b8',
-    medium: '#758894',
+    lower: '#f7f7f2',
+    low: '#bfbfbb',
+    medium: '#848481',
     high: '#5b6a74',
     higher: '#262e34',
-    highest: '#0c0c0c'
+    highest: '#000'
   }
 };
