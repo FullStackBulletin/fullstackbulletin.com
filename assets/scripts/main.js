ab22d0472ff439a2197e072e93087e29ee920334
AMangano <andreaman87@gmail.com>
2017-04-21 21:16:20 +0200

    Adds script to fix mini header during the page scrolling

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,19 @@
 window.addEventListener('load', function() {
   document.getElementById('layout').classList.remove('is-loading');
+
+  //set up ScrollMagic
+  const headerController = new ScrollMagic.Controller({
+    globalSceneOptions: {
+      triggerHook: 'onLeave'
+    }
+  });
+
+  //pin the navigation
+  const pin = new ScrollMagic.Scene({
+    triggerElement: '#main-ref',
+    offset: 200
+  })
+  // .setPin('#header-ref', {pushFollowers: false})
+    .setClassToggle('#header-ref', 'is-fixed')
+    .addTo(headerController);
 }, false);
