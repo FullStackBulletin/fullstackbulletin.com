64f61b87921522cc258b23db8c5cb7862703579c
Luciano Mammino <lucianomammino@gmail.com>
2024-02-11 10:59:16 +0000

    fixed references

--- webpack.production.js
+++ webpack.production.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const BUILDDIR = path.resolve(__dirname, process.env['npm_config_package_output']);
+const BUILDDIR = path.resolve(__dirname, process.env.PACKAGE_OUTPUT);
 
 module.exports = [{
   name: 'client',
